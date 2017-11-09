b68b3b097d55c033f23a88449298bf60a6923193
Patrick Hayes <phayes2828@gmail.com>
2017-11-08 17:47:13 -0800

    done

---
+++ CustomCell/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  CustomCell
+//
+//  Created by Patrick Hayes on 11/8/17.
+//  Copyright © 2017 Patrick Hayes. All rights reserved.
+//
+
+// we have to use UIKit to have access to UITableViewCell
+import UIKit
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightLabel: UILabel!
+}
